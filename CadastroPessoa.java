6922da004cb0eea109986eb807b898d4b3233f78
Letícia de Abreu <abreuhleticia@email.com>
2025-07-18 11:57:59 -0300

    Atualizado

--- CadastroPessoa.java
+++ CadastroPessoa.java
@@ -1,3 +1,5 @@
+public class CadastroPessoa{
+//Atributos
 private String nome;
 private String dataNascimento;
 private String email;
@@ -10,3 +12,26 @@ private int cep;
 private int cepf;
 private int matricula;
 private int numeroCelular;
+
+//Metodos
+//GET
+public String getnome(){
+    return this.nome;
+}
+
+public String getDataNascimento(){
+    return this.dataNascimento;
+}
+
+public void pessoaFazendo(){
+   System.out.println("Estou na faculdade");
+}
+
+public void vaiFazer(){
+ System.out.println("Estou indo para a faculdade");
+}
+
+public void comoFoi(){
+System.out.printl("A aula foi muito boa");
+}
+}
